37317db5d8251cfb8209f581192f85d76cc60898
paras <parasmanikc@gmail.com>
2022-12-07 17:43:13 +0545

    first commit

---
+++ src/components/FetchData.js
@@ -0,0 +1,8 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const FetchData = () => {
+  return <div>Fetch data</div>;
+};
+
+export default FetchData;
---
+++ src/components/RenderData.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const RenderData = () => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>lat</th>
+          <th>lon</th>
+          <th>time</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        <tr>
+          <td>0</td>
+          <td>0</td>
+          <td>0</td>
+        </tr>
+        <tr>
+          <td>0</td>
+          <td>0</td>
+          <td>0</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+};
+
+export default RenderData;
